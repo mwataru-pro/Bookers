180beae33e7b59a37ba1db3e145d0274ea5bc0ae
mwataru-pro <m1130wata@gmail.com>
2020-04-27 22:47:03 +0900

    [Finish] 4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -37,7 +37,8 @@ class BooksController < ApplicationController
     if @book.update(book_params)
     redirect_to book_path(@book.id), notice: "Book was successfully updated."
   else
-    render action: :edit
+    @books = Book.all
+    render "edit"
   end
 
   end
