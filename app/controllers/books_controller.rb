bf7e1bd849bdb717fc0607a68d3f4b36678e7b5e
mwataru-pro <m1130wata@gmail.com>
2020-04-27 19:55:12 +0900

    [Add] 遷移設定・バリデーション・サクセスメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,45 @@
 class BooksController < ApplicationController
+
+
   def top
   end
 
-  def new
-     @book = Book.new
-  end
 
   def index
-      @books = Book.all
-      @book = Book.new
+    @books = Book.all
+    @book = Book.new
   end
 
-  def show
-  end
 
   def create
-      book = Book.new(book_params)
-      book.save
-      redirect_to books_path
-
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id), notice: "Book was successfully created."
   end
+
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id), notice: "Book was successfully updated."
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path, notice: "Book was successfully destroyed."
+  end
+
 
   private
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   root 'books#top'
 
-  resources :books
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/books' => 'books#index'
+
+  post 'books' => 'books#create'
+
+  get 'books/:id' => 'books#show', as: 'book'
+
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+
+  patch 'books/:id' => 'books#update', as: 'update_book'
+
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  
 end
